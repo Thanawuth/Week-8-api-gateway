6bd9d0800f1220c3ea412702587e47bea20efc95
Aftertnw <64160049@go.buu.ac.th>
2024-09-09 21:43:47 +0700

    finish gateway

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ClientsModule, Transport } from '@nestjs/microservices'; // Import the 'Transport' symbol
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'MATH_SERVICE',
+        transport: Transport.TCP,
+        options: {
+          host: '127.0.0.1',
+          port: 8877,
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import axios from 'axios';
 
 @Injectable()
 export class AppService {
+  constructor(@Inject('MATH_SERVICE') private client: ClientProxy) {}
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async sum1(numbers: number[]) {
+    try {
+      const response = await axios.post('http://localhost:3001/math/sum', {
+        numbers,
+      });
+      return response.data;
+    } catch (error) {
+      console.log('Error calling the micro service', error);
+      throw error;
+    }
+  }
+
+  sum2(numbers: number[]) {
+    const patterns = { cmd: 'sum' };
+
+    return this.client.send<number>(patterns, numbers);
   }
 }
